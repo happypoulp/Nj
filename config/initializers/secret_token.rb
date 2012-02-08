d62cb51b93afe123ff63b38a452ca7bf54ad51e1
François Bonnefont <happypoulp@gmail.com>
2012-02-08 14:24:10 +0100

    [INIT] Create Nj rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nj::Application.config.secret_token = 'cfee6fcdf568ca69c76d4585bdd3e9f611992349c5f5b9abd3d0baaad3f523f332b9c7ecad1a029aedbc8542417be65c374b1c7a85549fddaede0465c4c7e910'
